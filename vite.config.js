06d5cc01416c90de5529c4119c6ea597bc88d711
enrixg <enricog@gmail.com>
2025-05-04 22:16:30 +0100

    ok

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/hangman/',        // ← tell Vite “all files live under /hangman/”
   plugins: [react()],
   server: {
     port: 5173,
